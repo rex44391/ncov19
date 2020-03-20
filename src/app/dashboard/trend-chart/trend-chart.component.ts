e5e6e58a05990c47433b26b1e9c0ac93268f9fbc
unknown <rex44391@gmail.com>
2020-03-20 15:18:43 +0800

    first commit

---
+++ src/app/dashboard/trend-chart/trend-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-trend-chart',
+    templateUrl: './trend-chart.component.html',
+    styleUrls: ['./trend-chart.component.css']
+})
+export class TrendChartComponent {
+    
+}
